2fe7a6be43144a9bc189435888abca2f12ffd68d
nowayrm <your.email@example.com>
2026-06-01 10:38:02 +0300

    some updates

--- app/neurofit/HeroNeurofit.tsx
+++ app/neurofit/HeroNeurofit.tsx
@@ -156,7 +156,7 @@ export default function HeroNeurofit() {
                       color: '#5A2345',
                     }}
                   >
-                    ДО КІНЦЯ ТРАВНЯ
+                    ДО КІНЦЯ ЧЕРВНЯ
                   </p>
                 </div>
 
